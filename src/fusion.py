39e9014f06049e300de05007d086aeba76526f2c
bohee111 <bboohhee06@gmail.com>
2025-05-13 10:51:01 +0900

    가중치 추가 fusion.py

--- src/fusion.py
+++ src/fusion.py
@@ -6,7 +6,8 @@ WildFusion 객체 생성 + calibration 수행
 def build_wildfusion(matcher_aliked, matcher_mega, calibration_query, calibration_db):
     fusion = WildFusion(
         calibrated_pipelines=[matcher_aliked, matcher_mega],
-        priority_pipeline=matcher_mega
+        priority_pipeline=matcher_mega,
+        weights=[0.3, 0.7]  # ALIKED, MEGAD 각각 가중치
     )
     fusion.fit_calibration(calibration_query, calibration_db)
     return fusion
