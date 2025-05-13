6001e482e478d08055105b3fc9be706b3614afb3
bohee111 <bboohhee06@gmail.com>
2025-05-13 11:34:14 +0900

    Update fusion.py

--- src/fusion.py
+++ src/fusion.py
@@ -3,7 +3,7 @@ from wildlife_tools.similarity.wildfusion import WildFusion
 '''
 WildFusion 객체 생성 + calibration 수행
 '''
-def build_wildfusion(matcher_aliked, matcher_mega, calibration_query, calibration_db):
+def build_wildfusion(matcher_aliked, matcher_loftr, matcher_mega, calibration_query, calibration_db):
     fusion = WildFusion(
         calibrated_pipelines=[matcher_aliked, matcher_mega],
         priority_pipeline=matcher_mega,
